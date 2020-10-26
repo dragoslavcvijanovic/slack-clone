bf5dd3a4372eeca7af2c6096cc37cba26ae74b46
Dragoslav Cvijanović <61536506+dragoslavcvijanovic@users.noreply.github.com>
2020-10-26 12:49:42 +0100

    Update index.js

--- function/index.js
+++ function/index.js
@@ -1,109 +1,129 @@
+const firebase = require("firebase");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const socketIo = require("socket.io");
+const http = require("http");
+const port = process.env.PORT || 4001;
 app.use(cors({ origin: true }));
+
+const server = http.createServer(app);
 
 var serviceAccount = require("./permissions.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://fir-api-9a206.firebaseio.com",
 });
+
 const db = admin.firestore();
 
-// create
-app.post("/api/create", (req, res) => {
-  (async () => {
-    try {
-      await db
-        .collection("items")
-        .doc("/" + req.body.id + "/")
-        .create({ item: req.body.item });
-      return res.status(200).send();
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
+const io = socketIo(server);
+// desi se kada se neko konektovao, uzima svoj soket
+io.on("connection", (socket) => {
+  console.log("New client connected: " + socket.id);
+  console.log(socket.request.sessionID);
 
-// read item
-app.get("/api/read/:item_id", (req, res) => {
-  (async () => {
-    try {
-      const document = db.collection("items").doc(req.params.item_id);
-      let item = await document.get();
-      let response = item.data();
-      return res.status(200).send(response);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
+  socket.on("reconnect", (socket) => {
+    console.log("Client reconnected: " + socket.id);
+  });
 
-// read all
-app.get("/api/read", (req, res) => {
-  (async () => {
-    try {
-      let query = db.collection("items");
-      let response = [];
-      await query.get().then((querySnapshot) => {
-        let docs = querySnapshot.docs;
-        for (let doc of docs) {
-          const selectedItem = {
-            id: doc.id,
-            item: doc.data().item,
-          };
-          response.push(selectedItem);
-        }
-        return response;
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+
+  socket.on("getRoomList", () => {
+    console.log("getRoomList");
+    const roomDb = db.collection("room");
+    let rez = [];
+    const b = roomDb.get().then((data) => {
+      data.forEach((doc) => {
+        rez.push({
+          id: doc.id,
+          name: doc.data().name,
+        });
       });
-      return res.status(200).send(response);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
+      io.emit("roomList", rez);
+      return true;
+    });
+  });
 
-// update
-app.put("/api/update/:item_id", (req, res) => {
-  (async () => {
-    try {
-      const document = db.collection("items").doc(req.params.item_id);
-      await document.update({
-        item: req.body.item,
+  //vrati podatke o konkretnoj osobi
+  socket.on("userLogin", (roomIdData) => {
+    console.log("user login: " + roomIdData.roomId);
+
+    const roomDb = db.collection("room").doc(roomIdData.roomId);
+    const a = roomDb.get().then((doc) => {
+      socket.emit("roomDetails", doc.data());
+      return true;
+    });
+
+    const messsagesDb = db
+      .collection("room")
+      .doc(roomIdData.roomId)
+      .collection("messages")
+      .orderBy("timestamp");
+    const b = messsagesDb.get().then((data) => {
+      let rez = [];
+      data.forEach((doc) => {
+        rez.push({
+          id: doc.id,
+          user: doc.data().user,
+          userImage: doc.data().userImage,
+          timestamp: doc.data().timestamp,
+          message: doc.data().message,
+        });
       });
-      return res.status(200).send();
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
+      socket.emit("userLoged", rez);
+      return true;
+    });
+  });
+
+  socket.on("addRoom", (newRoomData) => {
+    console.log("add room");
+    db.collection("room")
+      .add({
+        name: newRoomData.name,
+      })
+      .then((res) => {
+        const roomDb = db.collection("room");
+        let rez = [];
+        const b = roomDb.get().then((data) => {
+          data.forEach((doc) => {
+            rez.push({
+              id: doc.id,
+              name: doc.data().name,
+            });
+          });
+          io.emit("roomList", rez);
+          return true;
+        });
+      });
+  });
+
+  socket.on("sendMessage", (poruka) => {
+    console.log("New message arrived from client:" + poruka.channelId);
+
+    let data = JSON.parse(
+      JSON.stringify({
+        message: poruka.input,
+        timestamp: Date.now(),
+        user: poruka.user,
+        userImage: poruka.photoURL,
+      })
+    );
+
+    const a = db
+      .collection("room")
+      .doc(poruka.channelId)
+      .collection("messages")
+      .add(data)
+      .then((res) => {
+        data.id = res.id;
+        data.roomId = poruka.channelId;
+        io.emit("addMessageFromServer", data);
+        return true;
+      });
+  });
 });
-
-// delete
-app.delete("/api/delete/:item_id", (req, res) => {
-  (async () => {
-    try {
-      const document = db.collection("items").doc(req.params.item_id);
-      await document.delete();
-      return res.status(200).send();
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
-
-exports.app = functions.https.onRequest(app);
-//dodati socket oko API-ja
-
-
-
-/*app.get("/hello-world", (req, res) => {
-  return res.status(200).send("Hello World!");
-});
-*/
