898e3a6071a514a0fd7fe275145331d6a0f72862
Dragoslav Cvijanović <61536506+dragoslavcvijanovic@users.noreply.github.com>
2020-10-29 09:32:39 +0100

    Update Message.js

--- Components/Message.js
+++ Components/Message.js
@@ -1,22 +1,36 @@
 import React from "react";
 import "./Message.css";
-
-function Message({ message, timestamp, user, userImage }) {
-  return (
-    <div className="message">
-      <img src={userImage} alt="" />
-      <div className="message_info">
-        <h4>
-          {user}{" "}
-          <span className="message_timestamp">
-            {timestamp?.toUTCString()}
-            {/*greska timestamp.toUTCString is not function*/}
-          </span>
-        </h4>
-        <p>{message}</p>
+// {timestamp?.toUTCString()}
+function Message({ id, message, timestamp, user, userImage }) {
+  if (message.startsWith("<a")) {
+    let niz = message.split("|");
+    niz[1] = "http://localhost:3001" + niz[1];
+    return (
+      <div id={id} className="message">
+        <img src={userImage} alt="" />
+        <div className="message_info">
+          <h4>
+            {user} <span className="message_timestamp"></span>
+          </h4>
+          <p>
+            <a href={niz[1]}>{niz[2]}</a>
+          </p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div id={id} className="message">
+        <img src={userImage} alt="" />
+        <div className="message_info">
+          <h4>
+            {user} <span className="message_timestamp"></span>
+          </h4>
+          <p>{message}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Message;
